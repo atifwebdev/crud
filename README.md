04a191d2bee3e8b98fad13284a0466040452f365
Muhammad Atif <m.atifwebdeveloper@gmail.com>
2023-07-24 19:06:46 +0500

    created first hello program

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # crud
+
+1) Create server.mjs file
+2) install express js https://expressjs.com/en/starter/installing.html
+3) pakage.json > scripts > "start": "node server.mjs",
+4) install nodemon https://www.npmjs.com/package/nodemon
+5) Reload server every updated cmd (npm run dev)
--- helloworld/server.mjs
+++ helloworld/server.mjs
@@ -1,1 +1,25 @@
+// import pakages
+import express from "express";
 
+
+// Inotialize express app
+const app = express();
+
+// get request from localhost/3000
+app.get("/", (req, res) =>{
+    console.log("🚀 ~ file: server.mjs:7 ~ app.get ~ req:");
+    res.send({ message: "Hello World! by Ahmed" });
+});
+
+// passing params
+app.get("/name/:myName", (req, res) =>{
+    const name = req.params.myName;
+    console.log("🚀 ~ file: server.mjs:7 ~ app.get ~ req:", name);
+    res.send({ message: `New Hello World! by ${name}` });
+});
+
+const port = process.env.PORT || 3000;
+// app is listening from here
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
